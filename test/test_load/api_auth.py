599cae0298710213b3e9e27b69b96127469288d3
Siyang <bah@email.com>
2023-09-21 18:02:05 +0800

    add cognito token

--- test/test_load/api_auth.py
+++ test/test_load/api_auth.py
@@ -20,13 +20,16 @@ from locust import HttpUser, task, between
 host = "http://127.0.0.1:8000"
 payload = {"test": "something"}
 apikey = os.environ['API_KEY']
+cognito_token = os.environ['COGNITO_TOKEN']
+
 
 class DefineYourClassName(HttpUser):
     wait_time = between(3, 5)
     @task()
     def predict_endpoint(self):
         response = self.client.post(f"{host}/api", 
-                                    headers={"x-api-key": apikey}, 
+                                    headers={"x-api-key": apikey,
+                                             "Authorization": cognito_token}, 
                                     json=payload)
 
 
